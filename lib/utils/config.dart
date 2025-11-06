5d5a4b7d8425d5e65fb3b44c299f206720d8093a
Suyog-B20-Git <suyog7375@gmail.com>
2025-11-06 17:52:38 +0530

    url change

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -3,7 +3,8 @@ class Config {
   static const appName = 'E-Commerce';
 
   // Local URL
-  static const String domainUrl = 'http://192.168.43.201:3000/v1';
+  // static const String domainUrl = 'http://192.168.43.201:3000/v1';
   // Dynamic URl
-  // static const String domainUrl = 'https://vsm-backend-i6ad.onrender.com/v1';
+  static const String domainUrl =
+      'https://e-commerce-backend-ioqk.onrender.com/v1';
 }
